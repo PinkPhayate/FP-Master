a0126a33ce0777809bc1e5dfe2258f37d5022fed
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-17 22:18:22 +0900

    自動化に向けてコマンドライン変数の追加

--- src/python/lib/statistic.py
+++ src/python/lib/statistic.py
@@ -2,6 +2,7 @@ import metrics as me
 from scipy import stats
 # import figure as fig
 import configparser
+import sys
 
 # set environment lab or home
 inifile = configparser.SafeConfigParser()
@@ -10,11 +11,15 @@ ENV = inifile.get('env', 'locale')
 METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/JR/metrics-data/Apache-Derby'
 EXECUTION_MODE = inifile.get('env', 'mode')
 
+args = sys.argv
+v1 = args[1]
+v2 = args[2]
+
 # バージョンのメトリクスを比較する
 def compare_each_metrics():
-    # version1 = me.Metrics_Derby('10.9', METRICS_DIR)
+    # version1 = me.Metrics_Derby(v1, METRICS_DIR)
     # print(version1.mrg_df.ix[:,1])
-    version2 = me.Metrics_Derby('10.8', METRICS_DIR)
+    version2 = me.Metrics_Derby(v2, METRICS_DIR)
     print(version2.mrg_df)
     version2.export_df()
 
@@ -38,12 +43,8 @@ def compare_two_versions(version1, version2):
 
 # compare_each_metrics()
 
-version1 = me.Metrics_Derby('10.8', METRICS_DIR)
-version2 = me.Metrics_Derby('10.9', METRICS_DIR)
-version3 = me.Metrics_Derby('10.10', METRICS_DIR)
-print('10.8-10.9')
+version1 = me.Metrics_Derby(v1, METRICS_DIR)
+version2 = me.Metrics_Derby(v2, METRICS_DIR)
+# version3 = me.Metrics_Derby('10.10', METRICS_DIR)
+print(v1+'-'+v2)
 compare_two_versions(version1,version2)
-print('10.8-10.10')
-compare_two_versions(version1,version3)
-print('10.9-10.10')
-compare_two_versions(version2,version3)
