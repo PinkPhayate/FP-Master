11ff6d96976904fc4ed2d023f8177383e36aaf6f
PinkPhayate <pinqphayate@gmail.com>
2017-07-21 16:14:31 +0900

    execute one experiment

--- src/python/lib/statistic.py
+++ src/python/lib/statistic.py
@@ -1,7 +1,13 @@
-import metrics as me
+from lib import metrics as me
 from scipy import stats
 # import figure as fig
 import configparser
+
+THRESOLD = 0.05
+import configparser
+inifile = configparser.SafeConfigParser()
+inifile.read('./config.ini')
+EXECUTION_MODE = inifile.get('env', 'mode')
 
 # set environment lab or home
 inifile = configparser.SafeConfigParser()
@@ -15,14 +21,17 @@ def compare_each_metrics():
     # version1 = me.Metrics_Derby('10.9', METRICS_DIR)
     # print(version1.mrg_df.ix[:,1])
     version2 = me.Metrics_Derby('10.8', METRICS_DIR)
-    print(version2.mrg_df)
+    if EXECUTION_MODE == 'debug':
+        print(version2.mrg_df)
     version2.export_df()
 
     # fig.compare_metrics(filename=)
 
 def ks_2samp(m1, m2):
     ks_result = stats.ks_2samp(m1, m2)
-    print(ks_result.pvalue)
+    if EXECUTION_MODE == 'debug':
+        print(ks_result.pvalue)
+    return ks_result.pvalue
     # print(stats.ks_2samp(m1, m2))
 
 
@@ -30,20 +39,13 @@ def ks_2samp(m1, m2):
 
 def compare_two_versions(version1, version2):
     metrics = ['pd1','pd2','pd3','pd4','pd5','pd6','pc1','pc2','pc3','pc4']
+    alike_metrics = []
     for m in metrics:
-        print(m+': ',end='')
+        if EXECUTION_MODE == 'debug':
+            print(m+': ',end='')
         m1 = version1.mrg_df[m]
         m2 = version2.mrg_df[m]
-        ks_2samp(m1, m2)
-
-# compare_each_metrics()
-
-version1 = me.Metrics_Derby('10.8', METRICS_DIR)
-version2 = me.Metrics_Derby('10.9', METRICS_DIR)
-version3 = me.Metrics_Derby('10.10', METRICS_DIR)
-print('10.8-10.9')
-compare_two_versions(version1,version2)
-print('10.8-10.10')
-compare_two_versions(version1,version3)
-print('10.9-10.10')
-compare_two_versions(version2,version3)
+        pvalue = ks_2samp(m1, m2)
+        if (THRESOLD < pvalue):
+            alike_metrics.append(m)
+    return alike_metrics
