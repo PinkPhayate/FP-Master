e85ef84b023c1f814e27dc59fd26c62ddf4d7f5e
PinkPhayate <pinqphayate@gmail.com>
2017-07-21 16:23:50 +0900

    modify script in order to execute iterrable

--- src/python/ex01.py
+++ src/python/ex01.py
@@ -14,6 +14,9 @@ METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/JR/metrics-data/Apache-Derby'
 EXECUTION_MODE = inifile.get('env', 'mode')
 TARGET = ''
 
+args = sys.argv
+ITER = int(args[2]) if (len(args)>2) else (2000)
+
 def predict(ver, predict_ver,  alike_metrics):
     global TARGET
     if TARGET == 'Derby':
@@ -24,29 +27,37 @@ def predict(ver, predict_ver,  alike_metrics):
         # NO SERVUCE
         return
 
-    # RFN MODEL
-    sm = RandomOverSampler(ratio=0.2, random_state=random.randint(1,100))
-    X_resampled, y_resampled = sm.fit_sample( training_m.mrg_df, training_m.fault )
-    model = rf.train_rf( X_resampled, y_resampled )
-    rfn_value, importance = rf.predict_rf_saver(model, evaluate_m.mrg_df, evaluate_m.fault, TARGET + "-ex1rfn.csv")
-    print(rfn_value)
-    # acum_rfn_value += rfn_value
-    # diagram_list.append(rfn_value)
+    # initialize
+    acum_rfn_value=0
+    acum_intel_value=0
 
-    # INTELLIGENCE MODEL
-    sm = RandomOverSampler(ratio=0.2, random_state=random.randint(1,100))
-    alike_df = training_m.get_alike_df(alike_metrics)
-    X_resampled, y_resampled = sm.fit_sample( alike_df, training_m.fault )
-    model = rf.train_rf( X_resampled, y_resampled )
-    alike_df = evaluate_m.get_alike_df(alike_metrics)
-    rfn_value, importance = rf.predict_rf_saver(model, alike_df, evaluate_m.fault, TARGET + "-ex1rfn.csv")
-    print(rfn_value)
-    # acum_rfn_value += rfn_value
-    # diagram_list.append(rfn_value)
+    for i in range(ITER):
+        if i%500 == 0:
+            print('iteration: ' +str(i))
+
+        # RFN MODEL
+        sm = RandomOverSampler(ratio=0.2, random_state=random.randint(1,100))
+        X_resampled, y_resampled = sm.fit_sample( training_m.mrg_df, training_m.fault )
+        model = rf.train_rf( X_resampled, y_resampled )
+        rfn_value, importance = rf.predict_rf_saver(model, evaluate_m.mrg_df, evaluate_m.fault, TARGET + "-ex1rfn.csv")
+
+        acum_rfn_value += rfn_value
+        # diagram_list.append(rfn_value)
+
+        # INTELLIGENCE MODEL
+        sm = RandomOverSampler(ratio=0.2, random_state=random.randint(1,100))
+        alike_df = training_m.get_alike_df(alike_metrics)
+        X_resampled, y_resampled = sm.fit_sample( alike_df, training_m.fault )
+        model = rf.train_rf( X_resampled, y_resampled )
+        alike_df = evaluate_m.get_alike_df(alike_metrics)
+        rfn_value, importance = rf.predict_rf_saver(model, alike_df, evaluate_m.fault, TARGET + "-ex1rfn.csv")
+        acum_intel_value += rfn_value
+        # diagram_list.append(rfn_value)
+
+    print(acum_rfn_value/ITER)
+    print(acum_intel_value/ITER)
 
 
-
-args = sys.argv
 
 if args[1] == "derby":
     TARGET = 'Derby'
