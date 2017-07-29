6c2b0d0c385460587eb1bde218ecf18db06ed72f
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-29 16:33:38 +0900

    バグ数をカウントするラムダスクリプトを作成

--- src/python/tmp/script/bug_verify.py
+++ src/python/tmp/script/bug_verify.py
@@ -1,7 +1,29 @@
-import pandas
-buglist_filename = "/Users/phayate/Dropbox/STUDY/JR/metrics-data/Apache-Derby/bug-info/10.10.1.1-buglist.csv"
-metrics_filename = "/Users/phayate/src/ApacheDerby/metricsAll/mergedMetrics10.10.csv"
+import bug_process_merger as bg
+import sys
+import configparser
 
+# set environment lab or home
+inifile = configparser.SafeConfigParser()
+inifile.read('./../../config.ini')
+ENV = inifile.get('env', 'locale')
+METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/'
+derby_version = ['10.8.1.2', '10.9.1.0', '10.10.1.1', '10.11.1.1']
+solr_version = ['4.1.0', '4.2.0', '4.3.0', '4.4.0', '4.5.0']
 
+if __name__ == '__main__':
+    # args = sys.argv
+    # if 1 < len(args):
+    #     target = metrics_dir[1]
+    # else:
+    #     return
+    for elm in solr_version:
+        filename = METRICS_DIR + 'Solr/bug/slr_' + elm + '_bgmd.csv'
+        bug_list = bg.get_bug_list_solr(filename)
+        print('version : ' + elm)
+        print('numOfBug: ' + str(len(bug_list)))
 
-df = pd.read_csv(metrics_filename,)
+    for elm in derby_version:
+        filename = METRICS_DIR + 'Derby/bug/' + elm + '-buglist.csv'
+        bug_list = bg.get_bug_list_derby(filename)
+        print('version : ' + elm)
+        print('numOfBug: ' + str(len(bug_list)))
