31491cf4577c9ef4d198695efa6134b66699bbb2
Tanaka Hayate <pinqphayate@gmail.com>
2017-07-30 17:40:05 +0900

    メトリクスの分布を図表化するためのスクリプトを作成

---
+++ src/python/tmp/script/figure-boxplot.py
@@ -0,0 +1,44 @@
+import sys,os
+
+PACKAGE_PARENT = '../..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+import configparser
+import matplotlib.pyplot as plt
+from lib.metrics import Metrics_Origin
+
+inifile = configparser.SafeConfigParser()
+inifile.read('./config.ini')
+ENV = inifile.get('env', 'locale')
+
+
+def _create_boxplot_diagram(g1, g2, save_name, title=None):
+    # create graph just one version
+    hige = (g1, g2)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    bp = ax.boxplot(hige)
+    ax.set_xticklabels(['pre', 'cre'])
+
+    plt.grid()
+    plt.xlabel('VERSION')
+    plt.ylabel('DIAGRAM VALUE')
+    # plt.title(title)
+    plt.savefig(save_name)
+
+
+if __name__ == '__main__':
+    """
+    メトリクスの分布を箱ひげ図で表すスクリプト
+    """
+    v1 = '4.1.0'
+    v2 = '4.2.0'
+    METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Solr/all'
+    version1 = Metrics_Origin(v1, METRICS_DIR)
+    version2 = Metrics_Origin(v2, METRICS_DIR)
+    _v1 = version1.mrg_df[['pd1']]
+    _v2 = version2.mrg_df[['pd1']]
+    save_name = 'compare-wisky-pd1.png'
+    _create_boxplot_diagram(_v1, _v2, save_name)
