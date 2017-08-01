e7c62ff1a41484fc5a48be111d70524e14775633
Tanaka Hayate <pinqphayate@gmail.com>
2017-08-01 19:20:05 +0900

    バグ数の累積を取得するメソッドに変更

--- src/python/tmp/script/bug_process_merger.py
+++ src/python/tmp/script/bug_process_merger.py
@@ -28,18 +28,22 @@ if __name__ == '__main__':
     bug_filename = args[1]
     process_m_filename = args[2]
     save_filename = args[3]
-    # bug_list = get_bug_list_derby(bug_filename)
-    bug_list = get_bug_list_solr(bug_filename)
+    # derby
+    bug_list = get_bug_list_derby(bug_filename)
+    # solr
+    # bug_list = get_bug_list_solr(bug_filename)
     print("bug count:  " + str(len(bug_list)))
     df = pd.read_csv(process_m_filename,header=0)
     # derby
-    # df['fileName'] = df.apply(lambda x: x['fileName'].split("/")[9:], axis=1)
-
+    df['fileName'] = df.apply(lambda x: x['fileName'].split("/")[9:], axis=1)
     # sorl
-    df['fileName'] = df.apply(lambda x: x['fileName'].split("/")[6:], axis=1)
+    # df['fileName'] = df.apply(lambda x: x['fileName'].split("/")[6:], axis=1
+
     df['fileName'] = df.apply(lambda x: "/".join(x['fileName']), axis=1)
-    print(df['fileName'])
-    df['bug'] = df.apply(lambda x: 1 if(x['fileName'] in bug_list) else 0, axis=1)
+    # bug number is binary
+    # df['bug'] = df.apply(lambda x: 1 if(x['fileName'] in bug_list) else 0, axis=1)
+    # bug number is integer
+    df['bug'] = df.apply(lambda x: bug_list.count(x['fileName']), axis=1)
     found = df['bug'].sum()
     print("found bug: " + str(found))
     df.to_csv(save_filename)
