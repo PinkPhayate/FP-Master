773c4b85181cc981dfbc05b18b2b55c7858360eb
Tanaka Hayate <pinqphayate@gmail.com>
2017-09-18 06:27:14 +0900

    修正されたモジュールがどれほどFPと判定されているか調査するスクリプトの作成

---
+++ src/python/tmp/script/report_analyzer.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import configparser
+import matplotlib.pyplot as plt
+
+report_df = pd.read_csv('/Users/phayate/Dropbox/STUDY/Result/4.5.0nml_values.csv', header=0, index_col=0)
+def calculate_modified_fp(report_df):
+    """
+    FPと予測したモジュールのうち、修正のあったモジュールがどのくらいあったかを調査するスクリプト
+    50回試行したうち、修正モジュール数とFP数が異なっていたかをカウントする。
+    つまり、カウントが多いほど、修正モジュールがFPになっている可能性が高く、
+    """
+    #修正されたかつ、FPだったモジュールの数
+    modified_df = report_df[report_df['isModified'].apply(lambda x: x == 1)]
+    value_df = modified_df.ix[:, 3:]
+    s = value_df.apply(lambda x: sum(x))
+
+    # FPと予測したモジュールの個数
+    value_df = report_df.ix[:, 3:]
+    all_sum = value_df.apply(lambda x: sum(x))
+
+    d = pd.concat([s, all_sum], axis=1)
+    d = d.apply(lambda x: x[0]/x[1], axis=1)
+    return d, len(d[d.apply(lambda x: x < 1.0)])
+
+def calc_rate_fp_modified():
+    for version in versions:
+        for model_type in types:
+            filename = '/Users/phayate/Dropbox/STUDY/Result/{0}{1}_values.csv'\
+                                            .format(version, model_type)
+            try:
+                df = pd.read_csv(filename, header=0, index_col=0)
+                sr, num = calculate_modified_fp(report_df=df)
+                print('{0}-{1} {2}'.format(version, model_type, num))
+                lab = "{0}{1}".format(version, model_type)
+                left = range(len(sr))
+                height = sr.tolist()
+                plt.plot(left, height, label=lab)
+            except Exception as e:
+                print(e)
+                print('no file: {}'.format(filename))
+    plt.legend()
+    plt.show()
+
+# set environment lab or home
+inifile = configparser.SafeConfigParser()
+inifile.read('./config.ini')
+versions = inifile.get('version', 'derby').split(',')
+versions.extend(inifile.get('version', 'solr').split(','))
+types = inifile.get('model', 'model').split(',')
