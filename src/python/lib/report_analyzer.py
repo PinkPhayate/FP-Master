a0db02ccfaa43f2721657720c10cfada8ae7a735
Tanaka Hayate <pinqphayate@gmail.com>
2017-10-01 03:56:52 +0900

    value3, value4が計算されていなかったバグを修正

--- src/python/lib/report_analyzer.py
+++ src/python/lib/report_analyzer.py
@@ -71,6 +71,10 @@ class Analyzer(object):
         self.accum_val1.append(v1)
         # v3 = self.calculate_3()
         # self.accum_val3.append(v3)
+        v2 = self.calculate_2()
+        self.accum_val2.append(v2)
+        v3 = self.calculate_3()
+        self.accum_val3.append(v3)
         v4 = self.calculate_4()
         self.accum_val4.append(v4)
 
