5089de4553aae7a115f1067017aeade9adda52d3
Tanaka Hayate <pinqphayate@gmail.com>
2017-10-01 23:11:38 +0900

    各実験をマルチスレッドで実装するように変更

--- src/python/ex00.py
+++ src/python/ex00.py
@@ -19,20 +19,50 @@ def apply_solr_ex01():
     ex01.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Solr/all'
     ex01.exp_solr()
 
-if __name__ == '__main__':
-    start = time.time()
-    ex01.PRED_TYPE = 'rf'
-    ex01.ITER = 5
+rep = ex01
+def wrapper_exp(arg1, arg2):
+    rep.exp(arg1, arg2)
+
+def main():
+    derby_tuple = (('10.8', '10.9'), ('10.9', '10.10'))
+    solr_tuple  = (('4.1.0', '4.2.0'),
+                   ('4.2.0', '4.3.0'),
+                   ('4.3.0', '4.4.0'),
+                   ('4.4.0', '4.5.0'))
+    rep.PRED_TYPE = 'rf'
+    rep.ITER = 5
     jobs = []
-    job = Process(target=apply_derby_ex01)
-    jobs.append(job)
-    job.start()
 
-    job = Process(target=apply_solr_ex01)
-    jobs.append(job)
-    job.start()
+    rep.TARGET = 'Derby'
+    rep.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Derby/all'
+    for t in derby_tuple:
+        job = Process(target=wrapper_exp, args=t)
+        jobs.append(job)
+        job.start()
 
-    [job.join() for job in jobs]
+    rep.TARGET = 'Solr'
+    rep.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Solr/all'
+    for t in solr_tuple:
+        job = Process(target=wrapper_exp, args=t)
+        jobs.append(job)
+        job.start()
 
-    elapsed_time = time.time() - start
-    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+if __name__ == '__main__':
+    main()
+    # start = time.time()
+    # ex01.PRED_TYPE = 'rf'
+    # ex01.ITER = 5
+    # jobs = []
+    # job = Process(target=apply_derby_ex01)
+    # jobs.append(job)
+    # job.start()
+    #
+    # job = Process(target=apply_solr_ex01)
+    # jobs.append(job)
+    # job.start()
+    #
+    # [job.join() for job in jobs]
+    #
+    # elapsed_time = time.time() - start
+    # print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
