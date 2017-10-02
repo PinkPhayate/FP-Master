92735c3801142a9eb5d7a9ac9c8e0c591c3df072
PinkPhayate <pinqphayate@gmail.com>
2017-10-02 16:04:47 +0900

    ex3wo

--- src/python/ex00.py
+++ src/python/ex00.py
@@ -7,18 +7,6 @@ inifile = configparser.SafeConfigParser()
 inifile.read('./config.ini')
 ENV = inifile.get('env', 'locale')
 
-
-def apply_derby_ex01():
-
-    ex01.TARGET = 'Derby'
-    ex01.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Derby/all'
-    ex01.exp_derby()
-
-def apply_solr_ex01():
-    ex01.TARGET = 'Solr'
-    ex01.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Solr/all'
-    ex01.exp_solr()
-
 rep = ex01
 def wrapper_exp(arg1, arg2):
     rep.exp(arg1, arg2)
@@ -52,6 +40,36 @@ def main():
     elapsed_time = time.time() - start
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
+def main_ensemble():
+    global rep = ex03
+    start = time.time()
+    derby_tuple = (('10.8', '10.9'), ('10.9', '10.10'))
+    solr_tuple  = (('4.1.0', '4.2.0'),
+                   ('4.2.0', '4.3.0'),
+                   ('4.3.0', '4.4.0'),
+                   ('4.4.0', '4.5.0'))
+    rep.PRED_TYPE = 'svc'
+    rep.PRED_TYPE2 = 'rf'
+    rep.ITER = 5
+    jobs = []
+
+    rep.TARGET = 'Derby'
+    rep.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Derby/all'
+    for t in derby_tuple:
+        job = Process(target=wrapper_exp, args=t)
+        jobs.append(job)
+        job.start()
+
+    rep.TARGET = 'Solr'
+    rep.METRICS_DIR = '/Users/'+ENV+'/Dropbox/STUDY/Metrics/Solr/all'
+    for t in solr_tuple:
+        job = Process(target=wrapper_exp, args=t)
+        jobs.append(job)
+        job.start()
+
+    [job.join() for job in jobs]
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 if __name__ == '__main__':
     main()
     # start = time.time()
