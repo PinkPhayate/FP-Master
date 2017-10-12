3868cc9b02262fbc32cd3e54f4f79242dac16093
Tanaka Hayate <pinqphayate@gmail.com>
2017-10-13 07:40:35 +0900

    取得失敗時にログを出力する処理を実装

--- src/python/test_crwaler_derby.py
+++ src/python/test_crwaler_derby.py
@@ -4,9 +4,24 @@ import lxml
 
 
 def test_find_bug_module():
-    url = 'https://issues.apache.org/jira/secure/attachment/12861732/SOLR_10404.patch'
+    url = 'https://issues.apache.org//jira/secure/attachment/12551717/SOLR-4019.patch'
     source = request.urlopen(url)
     modules = cd.extract_bug_module_name(source)
     print(modules)
 
-test_find_bug_module()
+def test_skip_versions():
+    url = 'https://lucene.apache.org/solr/7_0_1/changes/Changes.html'
+    url_json = cd.get_fixed_bug_url(url)
+    print('before size: {}'.format(len(url_json.keys())))
+    url_json = cd.skip_version(url_json)
+    print('after size: {}'.format(len(url_json.keys())))
+
+def test_get_bug_module():
+    url = 'https://issues.apache.org//jira/secure/attachment/12551717/SOLR-4019.patch'
+    modules = cd.extract_bug_module_name(url)
+    print(modules)
+
+
+# test_find_bug_module()
+# test_skip_versions()
+test_get_bug_module()
