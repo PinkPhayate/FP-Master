84d92d8be7838f0c5a7552a3f9f0a9257056af08
Tanaka Hayate <pinqphayate@gmail.com>
2017-11-20 23:07:11 +0900

    各バージョンの主要インデックスを出力するメソッドを変更

--- src/python/count_module.py
+++ src/python/count_module.py
@@ -16,7 +16,15 @@ ENV = inifile.get('env', 'locale')
 
 
 def main():
+    import csv
+    f = open('sw_indecies.csv', 'w')
+    writer = csv.writer(f, lineterminator='\n')
+
     model_dict = model_creator.get_model_dictionary()
+    indecies_list = []
+    col_list = ['total modules', 'modified modules', 'bug modules',
+                'modified bug modules']
+    writer.writerow(col_list)
     for sw_name, models in model_dict.items():
         for model in models:
             print(sw_name, model.final_version)
@@ -31,14 +39,23 @@ def main():
             df = pd.concat([mrg_df, fault], axis=1)
             df = df.rename(columns = {df.columns[-1]:'fault'})
             # print(df)
-
+            li = ['{} {}'.format(sw_name, model.final_version)]
             # 総モジュール数
-            print('total module, {}'.format(len(df)))
-
+            print('total modules, {}'.format(len(df)))
+            li.append(len(df))
             # 変更モジュール数
-            print('modified module, {}'.format(len(modified_df)))
-            # 変更モジュールのうちの欠陥数
+            print('modified modules, {}'.format(len(modified_df)))
+            li.append(len(modified_df))
+
+            #  総欠陥数
             d = df[fault.apply(lambda x: x==1)]
-            print('bug of modified module, {}'.format(len(d)))
+            print('bug modules, {}'.format(len(d)))
+            li.append(len(d))
+
+            #  変更が合ったうちの総欠陥数
+            d = modified_df[fault.apply(lambda x: x==1)]
+            print('modified bug modules, {}'.format(len(d)))
+            li.append(len(d))
+            writer.writerow(li)
 
 main()
